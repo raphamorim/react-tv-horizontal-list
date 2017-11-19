4dd7b43ca8f90fc92769eabbd919c2ace66fabdf
Raphael Amorim <rapha850@gmail.com>
2017-11-19 03:07:52 -0200

    initial commit

---
+++ src/data.js
@@ -0,0 +1,57 @@
+export const animes = [
+  {
+    title: 'Cowboy Bebop',
+    thumbUrl: 'images/cowboy-bebop.jpg'
+  },
+  {
+    title: 'Sword of the Stranger',
+    thumbUrl: 'images/sword-of-the-stranger.jpg'
+  },
+  {
+    title: 'Steins;Gate',
+    thumbUrl: 'images/steins-gate.jpg'
+  },
+  {
+    title: 'Oreshura',
+    thumbUrl: 'images/oreshura.jpg'
+  },
+  {
+    title: 'Barakamon',
+    thumbUrl: 'images/barakamon.jpg'
+  },
+  {
+    title: 'Crossgame',
+    thumbUrl: 'images/crossgame.jpg'
+  },
+  {
+    title: 'Naruto',
+    thumbUrl: 'images/naruto.jpg'
+  }
+]
+
+export const movies = [
+  {
+    title: 'Baby Driver',
+    thumbUrl: 'images/baby-driver.jpg'
+  },
+  {
+    title: 'Blade Runner 2049',
+    thumbUrl: 'images/blade-runner-2049.jpg'
+  },
+  {
+    title: 'Dunkirk',
+    thumbUrl: 'images/dunkirk.jpg'
+  },
+  {
+    title: 'Thor: Ragnarok',
+    thumbUrl: 'images/thor-ragnarok.jpg'
+  },
+  {
+    title: 'Logan',
+    thumbUrl: 'images/logan.jpg'
+  },
+  {
+    title: 'Baywatch',
+    thumbUrl: 'images/baywatch.jpg'
+  }
+]
---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import ReactTV from 'react-tv'
+import { animes, movies } from './data'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div class="contain">
+        <h1>Animes</h1>
+        <div class="row">
+          <div class="row__inner">
+            {List(animes)}
+          </div>
+        </div>
+        <h1>Movies</h1>
+        <div class="row">
+          <div class="row__inner">
+            {List(movies)}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+function List(source) {
+  let list = []
+  source.forEach((item) => {
+    const tile = (
+      <div class="tile">
+        <div class="tile__media">
+          {
+            (item.thumbUrl)
+            ? <img class="tile__img" src={item.thumbUrl}/>
+            : null
+          }
+        </div>
+        <div class="tile__details"/>
+        <div class="tile__title">
+          {
+            (item.title)
+            ? item.title
+            : null
+          }
+        </div>
+      </div>
+    )
+
+    list.push(tile)
+  })
+
+  return list
+}
+
+ReactTV.render(<App/>, document.querySelector('#root'))
